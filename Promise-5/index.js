31e30b71ed83549be1e1f5ed8b338af6c14218b1
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-09-11 12:51:33 -0700

    challenge 5 complete

--- Promise-5/index.js
+++ Promise-5/index.js
@@ -1,6 +1,20 @@
 function whatToEat(timeOfDay) {
   // Create a new promise and return it here
-  
+  return new Promise((resolve, reject) => {
+    switch (timeOfDay) {
+      case 'breakfast':
+        resolve('Eggs');
+        break;
+      case 'lunch':
+        resolve('Sandwich');
+        break;
+      case 'dinner':
+        resolve('Pasta');
+        break;
+      default:
+        reject("I don't eat between meal snacks!");
+    }
+  });
 }
 
 // whatToEat should return a promise. 
@@ -10,8 +24,35 @@ function whatToEat(timeOfDay) {
 // the the message: "I don't eat between meal snacks!"
 
 // Handle the Promise returned from whatToEat here: 
+whatToEat('breakfast')
+  .then((food) => {
+    console.log(food);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 whatToEat('lunch')
+  .then((food) => {
+    console.log(food);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+whatToEat('dinner')
+  .then((food) => {
+    console.log(food);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // Handle this Promise: 
 whatToEat('elevens')
+  .then((food) => {
+    console.log(food);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
